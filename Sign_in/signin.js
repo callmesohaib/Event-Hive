212c1b78b1214fd2d77aa3acf434b39713322e36
Sohaib Ikram <2022CS621@student.uet.edu.pk>
2025-02-21 22:38:39 +0500

    HomePage

--- Sign_in/signin.js
+++ Sign_in/signin.js
@@ -1,31 +1,29 @@
 document.getElementById("signIn").addEventListener("submit", function (event) {
-    event.preventDefault();
-  
-    let formData = new FormData(this);
-    for (let [key, value] of formData.entries()) {
-      console.log(key, value); 
-    }
-  
-    fetch("signin.php", {
-      method: "POST",
-      body: formData,
+  event.preventDefault();
+
+  let formData = new FormData(this);
+  for (let [key, value] of formData.entries()) {
+    console.log(key, value);
+  }
+
+  fetch("signin.php", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      console.log(response);
+      return response.json();
     })
-      .then((response) => {
-        console.log(response); 
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data); 
-        if (data.success) {
-          
-          window.location.href = "/ep/Dashboard/dashboard.html";
-        } else {
-          
-          alert(data.message);
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("An error occurred, please try again later.");
-      });
+    .then((data) => {
+      console.log(data);
+      if (data.success) {
+        window.location.href = "/ep/Home/home.html";
+      } else {
+        alert(data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("An error occurred, please try again later.");
+    });
 });
